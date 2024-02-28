3fccb5cd665e0c1c0cffeae0107707048d1a1365
Layotu <theoneknownjames@gmail.com>
2024-02-28 14:34:07 -0600

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# pygame initialization stuff, based on docs
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+fps = 30
+
+# kinematics stuffs
+x, y = screen.get_width() / 2, screen.get_height() / 2
+Vx, Vy = 15, -15
+
+# arrays for list of forces
+Fx = [0]
+Fy = [9.8]
+
+# so that it can be paused
+running = False
+spaceIsPressed = False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def drawForce(horizontal, vertical):
+    pygame.draw.line(screen, (0, 0, 0), (x, y), (x + horizontal, x + vertical))
+    # pygame.draw.line(horizontal, vertical, horizontal + (horizontal - x) * 0.2, vertical + (vertical - y) * 0.2)
+
+looping = True
+while looping:
+    # if you press the close button or the escape button, quit the while loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            looping = False
+
+    # pause the game if you press space
+    if pygame.key.get_pressed()[pygame.K_SPACE] and not spaceIsPressed:
+        running = not running
+        spaceIsPressed = True
+    elif not pygame.key.get_pressed()[pygame.K_SPACE]:
+        spaceIsPressed = False
+
+    # kinematics stuff to update values
+    if running:
+        for i in Fx:
+            Vx += i / 4
+        for i in Fy:
+            Vy += i / 4
+
+        x += Vx
+        y += Vy
+
+    # drawing stuffs
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, (100, 100, 100), pygame.Rect(x - 25, y - 25, 50, 50))
+    for i in range(len(Fx)):
+        drawForce(Fx[i], Fy[i])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    clock.tick(fps)
+    pygame.display.flip()
+
+pygame.quit()
